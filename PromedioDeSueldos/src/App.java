824ae5caa7bbce4455d6691dae60b6cdef68f6b2
carlosvillalba06 <20243ds168@utez.edu.mx>
2025-09-11 18:20:21 -0600

    Agregar PromedioDeSueldos como carpeta normal

---
+++ PromedioDeSueldos/src/App.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        double suma = 0, promedio = 0;
+
+        List<Worker> trabajadores = new ArrayList<>();
+
+        trabajadores.add(new Worker(1, "Carlos", 2000.40));
+        trabajadores.add(new Worker(2, "Ricardo", 3000.70));
+        trabajadores.add(new Worker(3, "Pedro", 400.40));
+        trabajadores.add(new Worker(4, "Getsai", 1500.40));
+        trabajadores.add(new Worker(5, "Maximiliano", 700.40));
+        trabajadores.add(new Worker(6, "Kevin", 600.40));
+
+        for(Worker trabajador : trabajadores){
+
+            System.out.println("ID: " + trabajador.getId() + " Nombre: " + trabajador.getNombre() + " Sueldo: " + trabajador.getSueldo());
+
+            suma = suma + trabajador.getSueldo();
+        }
+
+        System.out.println(" ");
+
+        promedio = suma / 6;
+        System.out.println("Promedio de sueldos: " + promedio);
+
+
+        int i = 0;
+        while (i < trabajadores.size()){
+            if(trabajadores.get(i).getSueldo() < 1000){
+                trabajadores.remove(i);
+            } else{
+                i++;
+            }
+        }
+
+        System.out.println(" ");
+
+        for(Worker trabajador : trabajadores){
+
+        System.out.println("ID: " + trabajador.getId() + " Nombre: " + trabajador.getNombre() + " Sueldo: " + trabajador.getSueldo());
+
+    
+        }
+
+    
+    }
+}
---
+++ PromedioDeSueldos/src/Worker.java
@@ -0,0 +1,30 @@
+public class Worker {
+
+    private int id;
+    private String nombre;
+    private double sueldo;
+
+    public Worker (int id, String nombre, double sueldo){
+
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+
+    }
+
+    public int getId(){
+
+        return id;
+    }
+
+    public String getNombre(){
+
+        return nombre;
+    }
+
+    public double getSueldo(){
+
+        return sueldo;
+    }
+
+}
