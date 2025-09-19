49e178a2f3cb4b329f0d70a1564bd73ecc5f15fe
carlosvillalba06 <20243ds168@utez.edu.mx>
2025-09-19 08:51:18 -0600

    Se inicializa la variable a y b

--- PotenciaNumero/src/App.java
+++ PotenciaNumero/src/App.java
@@ -11,6 +11,7 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 2; 
     }
 }
