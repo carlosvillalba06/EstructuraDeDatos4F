ad0d831f5191bbca0f5532e42a6a2f20df2c5bc9
carlosvillalba06 <20243ds168@utez.edu.mx>
2025-09-19 08:27:44 -0600

    Se creo la recursividad para poder invertir la cadena

--- ImprimirCadena/src/App.java
+++ ImprimirCadena/src/App.java
@@ -1,4 +1,10 @@
 public class App {
+
+    public static String invertirCadena(String cadena){
+        if (cadena.isEmpty()) return cadena;
+        return invertirCadena(cadena.substring(1)) + cadena.charAt(0);
+    }
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
