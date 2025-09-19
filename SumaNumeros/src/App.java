a3c5bdb9083717a58c518640ee32fb65af69f8a6
carlosvillalba06 <20243ds168@utez.edu.mx>
2025-09-19 07:25:17 -0600

    Se crea la funcion para la recursividad

--- SumaNumeros/src/App.java
+++ SumaNumeros/src/App.java
@@ -1,5 +1,14 @@
 public class App {
    
+
+    public static int suma(int n){
+        if (n == 0)  return 0;
+
+        return n + suma(n - 1);
+
+    }
+
+
      public static void main(String[] args) throws Exception {
         
         
